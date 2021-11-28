12b250926db0185f3c2b459642aadcc3323af099
Tetracyl <seifaziz10@gmail.com>
2021-11-27 21:41:02 -0800

    the

--- javascript/index.js
+++ javascript/index.js
@@ -90,35 +90,98 @@ async function commands() {
                 accordion.id = `accordionPanelsStayOpen`;
 
                 for (let i = 0; i < value.length; i++) {
+                    let card = document.createElement('div');
+                    card.className = 'accordion-padding';
+
                     let accordionItem = document.createElement('div');
                     accordionItem.className = 'accordion-item';
 
                     let h2 = document.createElement('h2');
                     h2.className = 'accordion-header';
-                    h2.id = 'panelsStayOpen-heading' + i;
+                    h2.id = 'panelsStayOpen-heading' + value[i].name;;
 
                     let button = document.createElement('button');
                     button.type = 'button';
                     button.className = 'accordion-button collapsed';
                     button.setAttribute('data-bs-toggle', 'collapse');
-                    button.setAttribute('data-bs-target', `#panelsStayOpen-collapse${i}`);
-                    button.setAttribute('aria-controls', `panelsStayOpen-collapse${i}`);
+                    button.setAttribute('data-bs-target', `#panelsStayOpen-collapse${value[i].name}`);
+                    button.setAttribute('aria-controls', `panelsStayOpen-collapse${value[i].name}`);
                     button.setAttribute('aria-expanded', 'false');
-                    button.innerHTML = value[i].name;
-                    h2.appendChild(button);
+
+                    let buttonRow = document.createElement('div');
+                    buttonRow.className = 'row';
+
+                    let commandName = document.createElement('div');
+                    commandName.innerHTML = "<strong class=\"command-name-strong\">" + value[i].name + "</strong> " + (value[i].description.usage == 'No usage provided' ? '' : value[i].description.usage.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+                    commandName.className = 'accordion-command-name';
+
+                    let commandDescription = document.createElement('div');
+                    commandDescription.innerHTML = value[i].description.content;
+                    commandDescription.className = 'accordion-command-description';
 
                     let div2 = document.createElement('div');
-                    div2.id = 'panelsStayOpen-collapse' + i;
+                    div2.id = 'panelsStayOpen-collapse' + value[i].name;
                     div2.className = 'accordion-body collapse';
-                    div2.setAttribute('aria-labelledby', `panelsStayOpen-heading${i}`);
+                    div2.setAttribute('aria-labelledby', `panelsStayOpen-heading${value[i].name}`);
                     let div3 = document.createElement('div');
                     div3.className = 'accordion-body';
-                    div3.innerHTML = value[i].description.content;
-                    div2.appendChild(div3);
 
+                    let bodyRow = document.createElement('div');
+                    bodyRow.className = 'row';
+
+                    let aliases = document.createElement('div');
+                    aliases.innerHTML = "<strong class=\"aliases-strong\">" + "Aliases:" + '</strong> ' + ((value[i].aliases instanceof Array) ? value[i].aliases.join(", ") : value[i].aliases);
+                    aliases.className = 'accordion-aliases-content';
+
+                    const e = [];
+                    if (e instanceof Array) {
+                        for (let j = 0; j < value[i].description.examples.length; j++) {
+                            e.push(`ear ${value[i].name} ${value[i].description.examples[j]}`);
+                        }
+                    }
+                    let examples = document.createElement('div');
+                    examples.innerHTML = "<strong class=\"examples-strong\">" + "Examples:" + '</strong> ' + ((value[i].description.examples instanceof Array) ? e.join(", ") : `ear ${value[i].name}`);
+                    examples.className = 'accordion-examples-content';
+
+                    // go through value[i].permissions.botPermissions and make the underscore a space and capitalize the first letter of each word, then add send and read messages as permissions
+                    const bP = [];
+                    if (value[i].permissions.botPermissions instanceof Array) {
+                        for (let j = 0; j < value[i].permissions.botPermissions.length; j++) {
+                            bP.push(value[i].permissions.botPermissions[j].replace(/_/g, ' ').replace(/\w\S*/g, function (txt) {
+                                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                            }));
+                        }
+                        bP.push('Send Messages', 'Read Messages');
+                    }
+
+                    const uP = [];
+                    if (value[i].permissions.userPermissions instanceof Array) {
+                        for (let j = 0; j < value[i].permissions.userPermissions.length; j++) {
+                            uP.push(value[i].permissions.userPermissions[j].replace(/_/g, ' ').replace(/\w\S*/g, function (txt) {
+                                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                            }));
+                        }
+                    }
+
+                    let botPermissions = document.createElement('div');
+                    botPermissions.innerHTML = "<strong class=\"examples-strong\">" + "Bot Permissions:" + '</strong> ' + ((value[i].permissions.botPermissions instanceof Array && bP.length != 0) ? bP.join(", ") : 'N/A');
+                    botPermissions.className = 'accordion-examples-content';
+
+                    let userPermissions = document.createElement('div');
+                    userPermissions.innerHTML = "<strong class=\"examples-strong\">" + "User Permissions:" + '</strong> ' + ((value[i].permissions.userPermissions instanceof Array && uP.length != 0) ? uP.join(", ") : 'N/A');
+                    userPermissions.className = 'accordion-examples-content';
+
+                    bodyRow.append(aliases, examples, botPermissions, userPermissions);
+                    div3.appendChild(bodyRow);
+                    buttonRow.appendChild(commandName);
+                    buttonRow.appendChild(commandDescription);
+                    button.appendChild(buttonRow);
+                    h2.appendChild(button);
+                    div2.appendChild(div3);
                     accordionItem.appendChild(h2);
                     accordionItem.appendChild(div2);
-                    accordion.appendChild(accordionItem);
+                    card.appendChild(accordionItem);
+                    accordion.appendChild(card);
                 }
                 div.appendChild(accordion);
                 main.appendChild(div);
